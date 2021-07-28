e1453d59bfb12f18a800c4029d4c29c5a39fbcf0
Marsel <marsellima@gmail.com>
2021-07-28 13:46:13 -0300

    first commit

---
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -0,0 +1,113 @@
+import { Button, FormControlLabel, Switch, TextField } from '@material-ui/core';
+import React, { useContext, useState } from 'react';
+import ValidacoesCadastro from '../../contexts/ValidacoesCadastro';
+
+function DadosPessoais({ aoEnviar }) {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(false);
+  const [erros, setErros] = useState({ cpf: { valido: true, texto: '' } });
+
+  const validacoes = useContext(ValidacoesCadastro);
+  const validarCampos = (event) => {
+    const { name, value } = event.target;
+    const novoEstado = { ...erros };
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+  };
+
+  function enviarReq() {
+    for (let campo in erros) {
+      if (!erros[campo].valido) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (enviarReq()) {
+          aoEnviar({ nome, sobrenome, cpf, novidades, promocoes });
+        }
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
+        onBlur={validarCampos}
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
+        id="CPF"
+        name="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch
+            checked={promocoes}
+            onChange={(event) => {
+              setPromocoes(event.target.checked);
+            }}
+            name="promocoes"
+            color="primary"
+          />
+        }
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={
+          <Switch
+            checked={novidades}
+            onChange={(event) => {
+              setNovidades(event.target.checked);
+            }}
+            name="novidades"
+            color="primary"
+          />
+        }
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Próximo
+      </Button>
+    </form>
+  );
+}
+
+export default DadosPessoais;
---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,75 @@
+import { Button, TextField } from '@material-ui/core';
+import React, { useContext, useState } from 'react';
+import ValidacoesCadastro from '../../contexts/ValidacoesCadastro';
+
+function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [erros, setErros] = useState({ senha: { valido: true, texto: '' } });
+
+  const validacoes = useContext(ValidacoesCadastro);
+  const validarCampos = (event) => {
+    const { name, value } = event.target;
+    const novoEstado = { ...erros };
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+  };
+
+  function enviarReq() {
+    for (let campo in erros) {
+      if (!erros[campo].valido) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        if (enviarReq()) {
+          aoEnviar({ email, senha });
+        }
+      }}
+    >
+      <TextField
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+        id="email"
+        label="Email"
+        type="email"
+        required
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={senha}
+        onChange={(event) => {
+          setSenha(event.target.value);
+        }}
+        onBlur={validarCampos}
+        error={!erros.senha.valido}
+        helperText={erros.senha.texto}
+        id="senha"
+        name="senha"
+        label="Senha"
+        type="password"
+        required
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Próximo
+      </Button>
+    </form>
+  );
+}
+
+export default DadosUsuario;
